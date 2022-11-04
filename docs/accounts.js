51589b6ff4559ae6f22eba0ba72b44afec148111
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-11-05 09:05:14 +1100

    wip

--- docs/accounts.js
+++ docs/accounts.js
@@ -24,7 +24,7 @@ const Accounts = {
           <div class="mt-0 flex-grow-1">
           </div>
           <div class="mt-0 pr-1">
-            <b-button v-if="sync.section == null" size="sm" @click="syncIt(['importFromEtherscan'])" variant="link" v-b-popover.hover.top="'Sync accounts'"><b-icon-arrow-clockwise shift-v="+1" font-scale="1.2"></b-icon-arrow-clockwise></b-button>
+            <b-button v-if="sync.section == null" size="sm" @click="syncIt(['importFromEtherscan'])" variant="link" v-b-popover.hover.top="'Retrieve transactions for accounts'"><b-icon-arrow-clockwise shift-v="+1" font-scale="1.2"></b-icon-arrow-clockwise></b-button>
           </div>
           <div class="mt-1" style="width: 200px;">
             <b-progress v-if="sync.section != null" height="1.5rem" :max="sync.total" show-progress :animated="sync.section != null" :variant="sync.section != null ? 'success' : 'secondary'" v-b-popover.hover.top="'Click the button on the right to stop. This process can be continued later'">
