64f32b29252aa4b311e9741a5ceab9a4558886c8
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-11-07 11:50:42 +1100

    wip

--- docs/txInfo.js
+++ docs/txInfo.js
@@ -19,7 +19,60 @@ async function getTxInfo(txHash, item, provider) {
   console.log("tx: " + JSON.stringify(tx, null, 2));
   console.log("txReceipt: " + JSON.stringify(txReceipt, null, 2));
 
-  if (contract) {
+  // ETH transfers
+  if (txReceipt.gasUsed == 21000) {
+    console.log("ETH transfer");
+    results.summary = "Transferred " + ethers.utils.formatEther(tx.value) + "Ξ"; // + tx.to;
+  }
+
+  // ERC-721 setApprovalForAll
+  if (!results.summary && tx.data.substring(0, 10) == "0xa22cb465") {
+    console.log("ERC-721 setApprovalForAll");
+    const interface = new ethers.utils.Interface(ERC721ABI);
+    let decodedData = interface.parseTransaction({ data: tx.data, value: tx.value });
+    for (const event of txReceipt.logs) {
+      let log = interface.parseLog(event);
+      if (log.name == "ApprovalForAll") {
+        const tokenOwner = log.args[0]
+        const operator = log.args[1];
+        const approved = log.args[2];
+        if (approved) {
+          results.summary = "Approved transfer of " + tx.to.substring(0, 16) + " to " + operator.substring(0, 16);
+        } else {
+          results.summary = "Revoked transfer approval of " + tx.to.substring(0, 16) + " from " + operator.substring(0, 16);
+        }
+      }
+    }
+  }
+
+  // ERC-721 safeTransferFrom(address from, address to, uint256 tokenId)
+  if (!results.summary && tx.data.substring(0, 10) == "0x42842e0e") {
+    console.log("ERC-721 safeTransferFrom");
+    const interface = new ethers.utils.Interface(ERC721ABI);
+    let decodedData = interface.parseTransaction({ data: tx.data, value: tx.value });
+    console.log("decodedData: " + JSON.stringify(decodedData, null, 2));
+    for (const event of txReceipt.logs) {
+      let log = interface.parseLog(event);
+      console.log("log: " + JSON.stringify(log, null, 2));
+      // Transfer (index_topic_1 address from, index_topic_2 address to, index_topic_3 uint256 tokenId)
+      if (log.name == "Transfer") {
+        const from = log.args[0]
+        const to = log.args[1];
+        const tokenId = log.args[2];
+        results.summary = "Transferred ERC-721 " + tx.to.substring(0, 16) + ":" + tokenId + " from " + from.substring(0, 16) + " to " + to.substring(0, 16);
+      }
+    }
+  }
+
+  // TODO: ERC-721 transfer
+
+  // TODO: ERC-20 approve
+
+  // TODO: ERC-20 transfer
+
+
+
+  if (!results.summary && contract) {
     const interface = new ethers.utils.Interface(contract.abi);
     let decodedData = interface.parseTransaction({ data: tx.data, value: tx.value });
     console.log("decodedData: " + JSON.stringify(decodedData, null, 2));
@@ -50,7 +103,7 @@ async function getTxInfo(txHash, item, provider) {
       for (const event of txReceipt.logs) {
         if (event.address == item.to) {
           let log = interface.parseLog(event);
-          console.log("HERE log: " + JSON.stringify(log, null, 2));
+          // console.log("log: " + JSON.stringify(log, null, 2));
           if (log.name == "TextChanged") {
             const node = log.args[0];
             const key = log.args[1].hash;
@@ -58,6 +111,28 @@ async function getTxInfo(txHash, item, provider) {
             results.summary = "Set ENS node " + node.substring(0, 10) + " key: " + value + " value: " + decodedData.args[2];
           }
         }
+      }
+    } else if (contract.name == "OpenSeaTransferHelper") {
+      console.log("OpenSeaTransferHelper");
+      const interface = new ethers.utils.Interface(ERC721ABI);
+      const transfers = [];
+      for (const event of txReceipt.logs) {
+        console.log("event: " + JSON.stringify(event));
+        if (event.topics[0] == "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef") {
+          // let log = interface.parseLog(event);
+          // console.log("HERE log: " + JSON.stringify(log, null, 2));
+          const address = event.address;
+          const from = ethers.utils.getAddress("0x" + event.topics[1].substring(26));
+          const to = ethers.utils.getAddress("0x" + event.topics[2].substring(26));
+          const tokenId = event.data;
+          console.log("address: " + address);
+          console.log("from: " + from);
+          console.log("to: " + to);
+          console.log("tokenId: " + tokenId);
+          transfers.push({ address, from, to, tokenId });
+        }
+        console.log("transfers: " + JSON.stringify(transfers));
+        results.summary = "Transferred " + JSON.stringify(transfers);
       }
     } else if (contract.name == "Seaport") {
       // console.log("abi: " + JSON.stringify(contract.abi));
