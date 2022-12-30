908cfab674d2be24dce0c3e048e1ecd24673c390
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-12-30 15:00:04 +1100

    functionSelectionHandler wip

---
+++ docs/functionSelectorHandler.js
@@ -0,0 +1,10 @@
+const _FUNCTIONSELECTORHANDLER = [
+  {
+    functionSelectors: {
+      "0x3d6a32bd": "createTradeContract(address asset, uint256 buyPrice, uint256 sellPrice, uint256 units, bool buysTokens, bool sellsTokens)", // 0xA9F801f160fe6A866dD3404599350AbBCAA95274
+    },
+    process: function(txData, account, accounts, events, results) {
+      console.log("createTradeContract");
+    },
+  }
+];
