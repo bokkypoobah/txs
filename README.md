3b9b2ecc465fdfe5be161c9eb49bce6b67ed51f9
Luke Schoen <ltfschoen@gmail.com>
2023-03-04 23:07:48 +1100

    fix: fix browser warning of missing source maps and add script

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Txs
 
 [https://bokkypoobah.github.io/txs/](https://bokkypoobah.github.io/txs/)
+
+## Development Environment
+
+* Open the index.html file in a web browser
+```
+open -a "Google Chrome" ./docs/index.html
+```
+* Check browser console for any errors or warnings
+* Update dependencies used in ./docs/index.html if necessary including source maps from a CDN for browser debugging using breakpoints. 
---
+++ scripts/updateDependencies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Update dependencies and source maps
+
+# JS
+wget -NP ./docs/js https://cdnjs.cloudflare.com/ajax/libs/bignumber.js/4.0.0/bignumber.js.map
+wget -NP ./docs/js https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.15.0/bootstrap-vue.min.js.map
+wget -NP ./docs/js https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.15.0/bootstrap-vue-icons.min.js.map
+wget -NP ./docs/js https://cdnjs.cloudflare.com/ajax/libs/dexie/3.0.3/dexie.js.map
+
+# CSS
+wget -NP ./docs/css https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css.map
+wget -NP ./docs/css https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.15.0/bootstrap-vue.min.css.map
+wget -NP ./docs/css https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.15.0/bootstrap-vue-icons.min.css.map
